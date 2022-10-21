2e47cb0e796cd9f1bea98cccb971ab1343c550c6
Hugo Shiniti Nishimoto <hsnishimoto@gmail.com>
2022-10-21 10:06:18 -0300

    Arrumado animacao de tween e roupas

--- shrimp-clicker/src/components/CustomizeTab.vue
+++ shrimp-clicker/src/components/CustomizeTab.vue
@@ -13,7 +13,7 @@
             clickable
             class="q-my-sm" 
             v-ripple
-            @click="[!roupa.comprado ? purchaseRoupa(roupa.id, roupa.custoInit) : roupasStore.roupaAtiva = roupa.imagem]"
+            @click="[!roupa.comprado ? purchaseRoupa(roupa.id, roupa.custoInit, roupasStore.roupaAtiva) : roupasStore.roupaAtiva = roupa.imagem]"
         >
         <q-item-section avatar class="no-pointer-events">
           <q-avatar>
@@ -51,9 +51,10 @@ export default {
       return (playerStore.points >= cost)
     }
     
-    const purchaseRoupa = (id, cost) => {
+    const purchaseRoupa = (id, cost, imagen) => {
       roupasStore.addRoupa(id)
       playerStore.pointDecrement(cost)
+      roupasStore.roupaAtiva = imagen
     }
 
     return {
